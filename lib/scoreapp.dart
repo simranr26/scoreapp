cb7b5080d884c690476d9e3083cffb8bd8c01768
unknown <simranrijal26@gmail.com>
2022-07-19 17:07:45 +0545

    changes

---
+++ lib/scoreapp.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ScoreApp());
+}
+
+class ScoreApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "ScoreApp",
+      home: ScoreHome(),
+    );
+  }
+}
+
+class ScoreHome extends StatefulWidget {
+  const ScoreHome({Key? key}) : super(key: key);
+
+  @override
+  _ScoreHomeState createState() => _ScoreHomeState();
+}
+
+class _ScoreHomeState extends State<ScoreHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Score Changer"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
